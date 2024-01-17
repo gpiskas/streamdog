f5d254e3e7af3f4d1b4ee7b19ce3fe7c613dbdfa
Georgios Piskas <gpiskas@gmail.com>
2024-01-18 00:28:06 +0100

    dest

--- src/components/DropArea/Destroyable.tsx
+++ src/components/DropArea/Destroyable.tsx
@@ -6,15 +6,12 @@ export const Destroyable: Able = {
     render(moveable: MoveableManagerInterface<any, any>, React: Renderer) {
         const rect = moveable.getRect();
         const { pos2 } = moveable.state;
-        const button = React.createElement("span", {
-            className: "moveable-destroyable-button",
-            onClick: () => moveable.triggerEvent("onWarpEnd", {}) // hijacked event!
-        }, ["✖️"]);
         return React.createElement("div", {
-            className: "moveable-destroyable",
+            className: "moveable-destroyable-button",
+            onClick: () => moveable.triggerEvent("onWarpEnd", {}), // hijacked event!
             style: {
                 transform: `translate(${pos2[0]}px, ${pos2[1]}px) rotate(${rect.rotation}deg) translate(5px, 5px)`
             }
-        }, [button]);
+        }, ["X"]);
     },
 }
